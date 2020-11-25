7f9f9cf3339f89914eacfbe1b7319fdf90431462
mAsadSiddique <asadsiddique.a@gmail.com>
2020-11-25 10:31:38 +0500

    Boiler Plate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="red-queen_and_alice">
+        <img id="red-queen_and_alice_sprite" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x" alt="Alice and the Red Queen running to stay in place." />
+      </div>
+    </div>
+  );
+}
+
+export default App;
